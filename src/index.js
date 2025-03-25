94bf9ce4263d2914eb747c60c6b69c9a4f1ea1df
zorikun1 <gal.zormen@gmail.com>
2025-03-25 12:11:36 +0200

    Delete src/index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const App = () => {
-  return <div style={{ textAlign: "center", paddingTop: "3rem" }}><h1>המערכת פועלת! 💪</h1></div>;
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
